8bf36ec00697ae3afbe99d7ae85844250815a50e
HobertJ <13521079@std.stei.itb.ac.id>
2023-02-08 22:29:02 +0700

    add javabot

---
+++ JavaBot/src/main/java/Enums/ObjectTypes.java
@@ -0,0 +1,30 @@
+package Enums;
+
+public enum ObjectTypes {
+  PLAYER(1),
+  FOOD(2),
+  WORMHOLE(3),
+  GAS_CLOUD(4),
+  ASTEROID_FIELD(5),
+  TORPEDO_SALVO(6),
+  SUPER_FOOD(7),
+  SUPERNOVA_PICKUP(8),
+  SUPERNOVA_BOMB(9),
+  TELEPORTER(10),
+  SHIELD(11);
+
+
+  public final Integer value;
+
+  ObjectTypes(Integer value) {
+    this.value = value;
+  }
+
+  public static ObjectTypes valueOf(Integer value) {
+    for (ObjectTypes objectType : ObjectTypes.values()) {
+      if (objectType.value == value) return objectType;
+    }
+
+    throw new IllegalArgumentException("Value not found");
+  }
+}
---
+++ JavaBot/src/main/java/Enums/PlayerActions.java
@@ -0,0 +1,20 @@
+package Enums;
+
+public enum PlayerActions {
+  FORWARD(1),
+  STOP(2),
+  START_AFTERBURNER(3),
+  STOP_AFTERBURNER(4),
+  FIRE_TORPEDOES(5),
+  FIRE_SUPERNOVA(6),
+  DETONATE_SUPERNOVA(7),
+  FIRE_TELEPORT(8),
+  TELEPORT(8),
+  ACTIVATE_SHIELD(9);
+
+  public final Integer value;
+
+  private PlayerActions(Integer value) {
+    this.value = value;
+  }
+}
